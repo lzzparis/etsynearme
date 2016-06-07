59a86c24cf374708863423f0df9a62c39ca2a8a7
Lizzie Paris <lzz.paris@gmail.com>
2016-06-07 09:01:31 -0700

    more refactoring

--- js/app.js
+++ js/app.js
@@ -1,11 +1,9 @@
 $(document).ready(function(){
 
-	var latitude = 41.2592330;
-	var longitude = -96.0334950;
 	var params ={
 		api_key:"208jico8ty161pguni7ha87e",
-		lat: latitude,
-		lon: longitude,
+		lat:0,
+		lon:0,
 		limit:8
 	};
 
@@ -33,66 +31,12 @@ function getLocation(params,address){
 		params.lat = results[0].geometry.location.lat();
 		params.lon = results[0].geometry.location.lng();
 
-		pullEtsyStores(params);
+		getShops(params);
 
 	})
 
 }
 
-//TODO rename to doc method name findAllShops
-function pullEtsyStores (params){
-	$.ajax({
-		url:"https://openapi.etsy.com/v2/shops.js",	
-		data: params,
-		dataType:"jsonp",
-		type: "GET",
-		// callback:"callback",
-		success: function(data){
-			data.results.forEach(function(one){
-				displayShop(one);
-			});
-			console.log(data);	
-		},
-		error: function(jqXHR, error){ //this waits for the ajax to return with an error promise object
-		 console.log(jqXHR	);
-		 console.log(error);
-		}
-	});
-}
-
-function displayShop(shop){
-
-	var shopElem = $(".templates .single-shop").clone();
-
-	var shopId = shop.shop_id;
-
-	//TODO - figure out why this isn't returning anythings
-	// var shopElem = $(".templates .single-shop").clone();
-
-	var shopUrl = shop.url;
-	var shopName = shop.shop_name;
-	var baseUri = "https://openapi.etsy.com/v2/shops/:shop_id/listings";
-	var listingParams = {
-			api_key:"208jico8ty161pguni7ha87e", 
-			shop_id:shop.shop_id,
-			limit:10,
-			includes:"MainImage"
-		};
-
-	$("#results").append("<div id=\""+shop.shop_id+"\"class=\"single-shop\">"+
-	"<h2 class=\"shop-name\">"+
-		"<a href=\""+shopUrl+"\" target=\"_blank\">"+shopName+"</a>"+
-	"</h2>"+
-	"<ul class=\"listings-list\">");
-
-	// getFeatured(shop, listingUri, listingParams);
-
-
-	featuredUri=baseUri+"/featured.js";
-	var getFeatured = buildFeaturedHandler(baseUri,listingParams,shop);
-	etsyGet(featuredUri,listingParams,getFeatured);
-
-}
 
 function etsyGet(url,params,successHandle){
 	$.ajax({
@@ -107,8 +51,46 @@ function etsyGet(url,params,successHandle){
 		 console.log(error);
 		}
 	});
+}
+
+//TODO rename to documented method name findAllShops?
+function getShops (params){
+	var url = "https://openapi.etsy.com/v2/shops.js";
+	etsyGet(url,params,shopHandler);
+}
+
+function shopHandler(data){
+	data.results.forEach(displayShop);
+}
+
+function displayShop(shop){
+	var shopElem = $(".templates .single-shop").clone();
+	var shopId = shop.shop_id;
+	var shopUrl = shop.url;
+	var shopName = shop.shop_name;
+	var baseUri = "https://openapi.etsy.com/v2/shops/:shop_id/listings";
+	var listingParams = {
+			api_key:"208jico8ty161pguni7ha87e", 
+			shop_id:shop.shop_id,
+			limit:10,
+			includes:"MainImage"
+		};
+
+	$("#results").append("<div id=\""+shop.shop_id+"\"class=\"single-shop\">"+
+	"<h2 class=\"shop-name\">"+
+		"<a href=\""+shop.shop_url+"\" target=\"_blank\">"+shop.shop_name+"</a>"+
+	"</h2>"+
+	"<ul class=\"listings-list\">");
+
+	// getFeatured(shop, listingUri, listingParams);
+
+
+	featuredUri=baseUri+"/featured.js";
+	var getFeatured = buildFeaturedHandler(baseUri,listingParams,shop);
+	etsyGet(featuredUri,listingParams,getFeatured);
 
 }
+
 
 function buildFeaturedHandler(url,params,shop){
 	return function(data){
@@ -128,50 +110,6 @@ function buildActiveHandler(shop){
 }
 
 
-
-
-
-function getFeatured(shop, uri, params){
-	var requestUri = uri+"/featured.js";
-
-	$.ajax({
-		url:requestUri,	
-		data: params,
-		dataType:"jsonp",
-		type: "GET",
-		// callback:"callback",
-		success: function(data){
-			if(data.count)	displayListings(shop, data);
-			else getActive(shop);
-		},
-		error: function(jqXHR, error){ //this waits for the ajax to return with an error promise object
-		 console.log(jqXHR	);
-		 console.log(error);
-		}
-	});
-}
-
-function getActive(shop){
-	$.ajax({
-		url:"https://openapi.etsy.com/v2/shops/:shop_id/listings/active.js",	
-		data:{
-			api_key:"208jico8ty161pguni7ha87e", 
-			shop_id:shop.shop_id,
-			limit:10
-		},
-		dataType:"jsonp",
-		type: "GET",
-		// callback:"callback",
-		success: function(data){
-			displayListings(shop, data);
-		},
-		error: function(jqXHR, error){ //this waits for the ajax to return with an error promise object
-		 console.log(jqXHR	);
-		 console.log(error);
-		}
-	});
-}
-
 function displayListings(shop, listings){
 	var shopUrl = shop.url;
 	var shopName = shop.shop_name;
@@ -188,7 +126,4 @@ function displayListings(shop, listings){
 	
 	});
 }
-
-
-
 
